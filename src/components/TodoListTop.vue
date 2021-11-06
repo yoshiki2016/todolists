99eac380c29765da41ec183ce4cede29c83886e4
Yoshiki2016 <785431ys@gmail.com>
2021-11-06 16:52:07 +0900

    Todoリストのトップ画面作成

---
+++ src/components/TodoListTop.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="todo-list-top">
+    <ul>
+      <li>Title</li>
+      <li>Status</li>
+      <li>
+        <button>編集</button>
+      </li>
+      <li>
+        <button>削除</button>
+      </li>
+    </ul>
+  </div>
+</template>
