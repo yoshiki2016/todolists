961ad5f00c86b42a2133eb54da271db3a4057f94
Yoshiki2016 <785431ys@gmail.com>
2021-11-06 17:57:58 +0900

    Todolistの追加画面と編集画面作成

---
+++ src/components/TodoListAdd.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="todo-list-top">
+    <input type="text" placeholder="タイトル" />
+    <select>
+      <option
+        v-for="statusItem in statusItems"
+        :value="statusItem"
+        :key="statusItem"
+      >
+        {{ statusItem }}
+      </option>
+    </select>
+    <button>追加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      statusItems: ["未着手", "着手", "完了"],
+    };
+  },
+};
+</script>
